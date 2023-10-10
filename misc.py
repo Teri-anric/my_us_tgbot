bdc9d3632eabd5da92607e6928ebc17238f9092e
Teri <75248049+Teri-anric@users.noreply.github.com>
2023-10-11 02:16:31 +0300

    update setting client

--- misc.py
+++ misc.py
@@ -3,7 +3,8 @@ from pyrogram import Client
 from config import API_ID, API_HASH, CLIENT_NAME
 from tts import STTWorker
 
-app = Client(CLIENT_NAME, api_id=API_ID, api_hash=API_HASH)
+app = Client(CLIENT_NAME, api_id=API_ID, api_hash=API_HASH,
+             app_version="Terigram 0.0.1", device_model="UB")
 stt = STTWorker()
 
 import handlers
