848de3ad452addca8bc5d39578ee50191ae22bdc
Teri <75248049+Teri-anric@users.noreply.github.com>
2023-10-21 14:35:39 +0300

    add tag all type user

--- handlers/userteg.py
+++ handlers/userteg.py
@@ -1,22 +1,21 @@
 import html
 
 from pyrogram import filters, Client
-from pyrogram.enums import ParseMode
-from pyrogram.types import Message
+from pyrogram.enums import ParseMode, ChatMembersFilter
+from pyrogram.types import Message, Chat
 
 from misc import app
 from utils import rand_emoji
 
 
-@app.on_message(filters.command(["all"], prefixes='!', case_sensitive=True) & filters.me & filters.group)
-async def ban_me_please(cl: Client, m: Message):
+async def teg_users(cl: Client, m: Message, mention_func=None, filter=enums.ChatMembersFilter.SEARCH):
     args = m.text.split(maxsplit=1)
     text = ""
     if len(args) == 2:
         text = html.escape(args[-1])
         text += "\n"
     users = []
-    async for member in m.chat.get_members(limit=100):
+    async for member in m.chat.get_members(limit=100, filter=filter):
         if not member.user:
             continue
         if member.user.is_bot or member.user.is_deleted:
@@ -24,8 +23,28 @@ async def ban_me_please(cl: Client, m: Message):
         users.append(member.user)
     # generate text
     for user in users:
-        mention_text = rand_emoji()
-        link = user.mention
-        text += link(mention_text, style=ParseMode.HTML)
+        if mention_func is None:
+            mention_text = None
+        else:
+            mention_text = mention_func(user)
+        text += user.mention(mention_text, style=ParseMode.HTML)
     await cl.send_message(m.chat.id, text=text, reply_to_message_id=m.reply_to_message_id,
                           parse_mode=ParseMode.HTML)
+
+
+@app.on_message(filters.command(["all"], prefixes='!', case_sensitive=True) & filters.me & filters.group)
+async def teg_first_100_users(cl: Client, m: Message):
+    func = lambda chat: rand_emoji()
+    await teg_users(cl, m, func)
+
+@app.on_message(filters.command(["tegadmin"], prefixes='!', case_sensitive=True) & filters.me & filters.group)
+async def teg_admins(cl: Client, m: Message):
+    await teg_users(cl, m, mention_func=None, filter=ChatMembersFilter.ADMINISTRATORS)
+
+@app.on_message(filters.command(["tegbot"], prefixes='!', case_sensitive=True) & filters.me & filters.group)
+async def teg_admins(cl: Client, m: Message):
+    await teg_users(cl, m, mention_func=None, filter=ChatMembersFilter.BOTS)
+
+@app.on_message(filters.command(["tegban"], prefixes='!', case_sensitive=True) & filters.me & filters.group)
+async def teg_admins(cl: Client, m: Message):
+    await teg_users(cl, m, mention_func=None, filter=ChatMembersFilter.BANNED)
